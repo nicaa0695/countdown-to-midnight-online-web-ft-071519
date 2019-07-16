9e971230b60c6d777974eb8bed0f4e0b2c8a3038
Veronica Lopez <nicaa0695@gmail.com>
2019-07-16 18:45:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,4 @@
 #write your code here
-countdown_output = 100
-def countdown
-  while seconds_left > 0 
-  puts "#{seconds_left} SECOND(S)!"
-  seconds_left -= 1 
-  puts "HAPPY NEW YEAR!"
-  end
-end
-
 def countdown
   countdown_output = 6
   while countdown_output > 0
