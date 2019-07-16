5b9151268e914bae9110457411ed5748ec54e2a5
Veronica Lopez <nicaa0695@gmail.com>
2019-07-16 18:45:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,11 @@ def countdown
   end
 end
 
-
+def countdown
+  countdown_output = 6
+  while countdown_output > 0
+  puts "#{countdown_output} SECOND(S)!"
+  countdown_output -= 1
+  puts "HAPPY NEW YEAR!"
+  end
+end
